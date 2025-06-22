bee687360271ab6a17f69dbb2455d5109807c43f
piyushrokaya <piyushrokaya600@gmail.com>
2025-06-22 22:51:08 +0545

    error blockchain ma matra halka

---
+++ Downloads/final/blockchain/index.js
@@ -0,0 +1,75 @@
+const crypto = require('crypto');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return crypto.createHash('sha256')
+            .update(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce)
+            .digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        const target = Array(difficulty + 1).join("0");
+        
+        while (this.hash.substring(0, difficulty) !== target) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log(`Block mined: ${this.hash}`);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, Date.now(), "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Example usage
+const myBlockchain = new Blockchain();
+
+myBlockchain.addBlock(new Block(1, Date.now(), { amount: 4 }));
+myBlockchain.addBlock(new Block(2, Date.now(), { amount: 10 }));
+
+console.log('Blockchain valid?', myBlockchain.isChainValid());
+console.log(JSON.stringify(myBlockchain, null, 4));
